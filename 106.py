631e5b13f6d05e155910789348ed85bb5d3a4de7
ajoke-ogo <81751017+ajoke-ogo@users.noreply.github.com>
2021-04-03 15:18:34 +0100

    Add files via upload

---
+++ 106.py
@@ -0,0 +1,25 @@
+print('Question one')
+print("name:Abdulganiyu Zainab")
+print("department: Science Education physics")
+print('question two')
+scare=('boo!')
+print(scare)
+print ('question three')
+Z1=5
+Z2=2
+Z3=Z1 +Z2
+print(Z1, Z2, Z3)
+print('Question four')
+Result =12/.5
+print(Result)
+print('Question five')
+a=7+5
+print(a)
+print('question six')
+Z=8%3
+print(Z)
+print('question seven')
+Result1=2*4
+Result2=4+2
+Result3=Result1* Result2
+print(Result3)
